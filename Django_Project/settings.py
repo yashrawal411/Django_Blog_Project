38952894441d68ae4f2e1739cbc75314d1c2a2d8
Yash Rawal <yashrawalcodes@gmail.com>
2022-02-26 16:38:55 +0530

    OK

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import django_heroku
 import tkinter
+import _tkinter
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
