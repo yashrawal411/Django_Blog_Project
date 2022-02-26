0d17e0c4958df5e1ff8eff40b83aeb9901640013
Yash Rawal <yashrawalcodes@gmail.com>
2022-02-26 18:25:07 +0530

    OK

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-
-
-
-
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,7 +26,7 @@ SECRET_KEY = 'django-insecure-5*+wc=!5bau(0ge_y-%g5k7r-@=@$kkctof4i+af=_0eyq9d(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mydjangoblogproj.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -145,3 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
